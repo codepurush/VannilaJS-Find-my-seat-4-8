42b7b261830f9105d27e8d08efe12ce9ca489696
Purushothaman Elango <codepurush@gmail.com>
2017-02-11 19:49:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Where x is number of rows and y is number of seats. This theatre has 26 rows and 100 seats per row. Objective is to print out pairs of row number & seatnumber
 for (var x = 0; x < 26; x++) {
     for (var y = 0; y < 100; y++) {
         console.log(x, y);
